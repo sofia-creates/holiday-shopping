bac13a3ae052196e37c5b86a1fdb22898fa69107
sofia-creates <sofia.ledin2@gmail.com>
2024-12-06 20:55:25 +0100

    refactored aside, next up making smaller components

--- frontend/src/Components/Aside.tsx
+++ frontend/src/Components/Aside.tsx
@@ -5,57 +5,15 @@ const Aside = () => {
     <aside>
         <h2>Christmas products</h2>
         <h3>Filter</h3>
-
+        
         <h4>Categories</h4>
-
-        {(categories.map((category:string)=>{
-            <p>hej</p>
-        })) }
-
-       {categories ? ( categories.map((category: string)=>(
+        {categories.map((category: string)=>(
             <div>
                 <input type="checkbox" name=""  value={category.replace(/\s+/g, '_').toLowerCase()} id="" />
                 <label htmlFor="christmasDecor">{category}</label>
-            </div>)
-            ) : (
-        <p>No categories found.</p>
-       )}
-
-        <div>
-            <input type="checkbox" name=""  value="christmasDecor" id="" />
-            <label htmlFor="christmasDecor">Christmas decorations</label>
-        </div>
-
-        <div>
-            <input type="checkbox" name=""  value="lighting" id="" />
-            <label htmlFor="lighting">Lighting</label>
-        </div>
-
-        <div>
-            <input type="checkbox" name=""  value="homeDecor" id="" />
-            <label htmlFor="lighting">Home decor</label> 
-        </div>
-
-        <div>
-            <input type="checkbox" name=""  value="kitchenware" id="" />
-            <label htmlFor="kitchenware">Kitchenware</label>
-        </div>
-
-        <div>
-            <input type="checkbox" name=""  value="apparel" id="" />
-            <label htmlFor="apparel">Apparel</label>
-        </div>
-
-        <div>
-            <input type="checkbox" name=""  value="electronics" id="" />
-            <label htmlFor="electronics">Electronics</label>
-        </div>
-
-        <div>
-            <input type="checkbox" name=""  value="homeFragrance" id="" />
-            <label htmlFor="homeFragrance">Home Fragrance</label>
-        </div>
-
+            </div>
+        )
+        )}
 
         <h4>Price</h4>
         <input type="checkbox" name="" id="" />
